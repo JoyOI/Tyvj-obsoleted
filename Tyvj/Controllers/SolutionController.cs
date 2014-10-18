5349a6abf5b49d6bc8e02b0149950d306fc72395
Gasai Yuno <1@4321.io>
2014-10-18 19:06:42 +0800

    solution create

--- Tyvj/Controllers/SolutionController.cs
+++ Tyvj/Controllers/SolutionController.cs
@@ -48,5 +48,46 @@ namespace Tyvj.Controllers
             };
             return View(problem);
         }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult SetTag(int id, int tid)
+        {
+            var solution = DbContext.Solutions.Find(id);
+            if (solution.Problem.Hide && (CurrentUser == null || (CurrentUser.ID != solution.UserID && CurrentUser.ID != solution.Problem.UserID && !IsMaster())))
+                return Content("Failed");
+            var tags = solution.SolutionTags.Where(x => x.AlgorithmTagID == tid).ToList();
+            if (tags.Count == 0)
+            {
+                var tag = new SolutionTag
+                {
+                    AlgorithmTagID = tid,
+                    SolutionID = id
+                };
+                DbContext.SolutionTags.Add(tag);
+                DbContext.SaveChanges();
+                return Content("Added");
+            }
+            else
+            {
+                foreach (var tag in tags)
+                    DbContext.SolutionTags.Remove(tag);
+                DbContext.SaveChanges();
+                return Content("Deleted");
+            }
+        }
+
+        [Authorize]
+        public ActionResult EditTags(int id)
+        {
+            var solution = DbContext.Solutions.Find(id);
+            if (solution.Problem.Hide && (CurrentUser == null || (CurrentUser.ID != solution.UserID && CurrentUser.ID != solution.Problem.UserID && !IsMaster())))
+                return Message("您无权执行本操作");
+            ViewBag.Tags = (from at in DbContext.AlgorithmTags
+                            where at.FatherID == null
+                            orderby at.ID ascending
+                            select at).ToList();
+            return View(solution);
+        }
     }
 }
--- Tyvj/Scripts/tyvj.extend.js
+++ Tyvj/Scripts/tyvj.extend.js
@@ -76,6 +76,18 @@ function StandingsUpdate(data) {
     standings.sort(cmp);
     StandingsDisplay();
 }
+function SetSolutionTag(tid) {
+    $.post("/Solution/SetTag/" + id, { tid: tid, rnd: Math.random() }, function (data) {
+        if (data == "Added") {
+            $("#t_" + tid).removeClass("gray");
+            $("#t_" + tid).addClass("orange");
+        }
+        else if (data == "Deleted") {
+            $("#t_" + tid).removeClass("orange");
+            $("#t_" + tid).addClass("gray");
+        }
+    });
+}
 
 function Lock() {
     lock = true;
@@ -237,7 +249,6 @@ function LoadStandings() {
         });
     }
 }
-
 function LoadReplies() {
     if ($("#discuss_detail_list").length > 0) {
         $.post("/Reply/GetReplies/", {
@@ -275,7 +286,6 @@ function LoadReplies() {
         });
     }
 }
-
 function LoadRanks()
 {
     if ($("#lstRanks").length > 0)
