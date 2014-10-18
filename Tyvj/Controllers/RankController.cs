a6e783f8284b206f8b3aba4ff64d0f5a34ee0a3e
Ivy-End <admin@ivy-end.com>
2014-10-18 15:37:47 +0800

    Ranks fix

--- Tyvj/Controllers/RankController.cs
+++ Tyvj/Controllers/RankController.cs
@@ -17,21 +17,14 @@ namespace Tyvj.Controllers
         }
 
         [HttpGet]
-        public ActionResult GetRanks(int page)
+        public ActionResult GetRanks(int page, int type)
         {
             var users = (from u in DbContext.Users
-                         orderby u.Ratings.Sum(x => x.Credit) descending
-<<<<<<< HEAD
-                         select u).Skip(12 * page).Take(12).ToList();
-            List<Rating> ratings = new List<Rating>();
-            for (int i = 0; i < users.Count(); i++)
-                ratings.Add(new Rating(users[i], page * 12 + i + 1));
-=======
-                         select u).Skip(10 * page).Take(10).ToList();
+                             orderby u.Ratings.Sum(x => x.Credit) descending
+                             select u).Skip(10 * page).Take(10).ToList();
             List<vRank> ratings = new List<vRank>();
-             for (int i = 0; i < users.Count(); i++)
-                 ratings.Add(new vRank(users[i], page * 10 + i + 1));
->>>>>>> ae09403613a823f0b149935f257a8dd4c97ffa27
+            for (int i = 0; i < users.Count(); i++)
+                ratings.Add(new vRank(users[i], page * 10 + i + 1));
             return Json(ratings, JsonRequestBehavior.AllowGet);
         }
     }
