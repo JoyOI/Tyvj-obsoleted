7ae6da7f88769ecb8d03d92b15c4a1c2c79b1402
あまみや ゆうこ <1@1234.sh>
2017-07-19 18:54:40 +0800

    Fix

--- Tyvj/Controllers/StatusController.cs
+++ Tyvj/Controllers/StatusController.cs
@@ -169,10 +169,11 @@ namespace Tyvj.Controllers
             var submitlist = Helpers.AcList.GetList(user.SubmitList);
             submitlist.Add(problem.ID);
             user.SubmitList = Helpers.AcList.ToString(submitlist);
-            var testcase_ids = (from tc in problem.TestCases
+            var _testcase_ids = (from tc in problem.TestCases
                                 where tc.Type != TestCaseType.Sample
                                 orderby tc.Type ascending
-                                select tc.ID).ToList();
+                                select new { tc.ID, tc.InputBlobId }).ToList();
+            var testcase_ids = _testcase_ids.Select(x => x.ID).ToList();
             if (contest_id != null)
             {
                 if (DateTime.Now < contest.Begin || DateTime.Now >= contest.End || contest.Format == ContestFormat.ACM || contest.Format == ContestFormat.OI)
@@ -232,12 +233,12 @@ namespace Tyvj.Controllers
             }
             else // 不是比赛任务
             {
-                foreach (var id in testcase_ids)
+                foreach (var id in _testcase_ids)
                 {
                     DbContext.JudgeTasks.Add(new JudgeTask
                     {
                         StatusID = status.ID,
-                        TestCaseID = id,
+                        TestCaseID = id.ID,
                         Result = JudgeResult.Pending,
                         MemoryUsage = 0,
                         TimeUsage = 0,
@@ -277,6 +278,7 @@ namespace Tyvj.Controllers
                         }
                         blobs.Add(new BlobInfo { Id = Guid.Parse(jt.TestCase.InputBlobId), Name = "input_" + caseIndex + ".txt" });
                         blobs.Add(new BlobInfo { Id = Guid.Parse(jt.TestCase.OutputBlobId), Name = "output_" + caseIndex + ".txt" });
+                        blobs = blobs.DistinctBy(x => x.Id).ToList();
                         ++caseIndex;
                     }
                     var stateMachineId = await client.PutStateMachineInstanceAsync("TyvjJudgeStateMachine", "http://tyvj.cn", blobs); // 创建StateMachine实例
@@ -303,6 +305,194 @@ namespace Tyvj.Controllers
             SignalR.UserHub.context.Clients.Group("Status").onStatusChanged(new vStatus(status));
             GC.Collect();
             return Content(status.ID.ToString());
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Benchmark(int problem_id = 1000, string code = "#include<iostream>\r\nusing namespace std;\r\nint main()\r\n{\r\nwhile(1);\r\nreturn 0;\r\n}", int language_id = 1, int? contest_id = null)
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                var problem = DbContext.Problems.Find(problem_id);
+                var user = (User)ViewBag.CurrentUser;
+                if (problem == null || (problem.Hide && !IsMaster() && CurrentUser.ID != problem.ID && !contest_id.HasValue))
+                    return Content("Problem not existed");
+                if (problem.VIP && user.Role < UserRole.VIP && problem.UserID != user.ID)
+                    return Content("Need VIP");
+                Contest contest = new Contest();
+                if (contest_id != null)
+                {
+                    contest = DbContext.Contests.Find(contest_id.Value);
+                    if (DateTime.Now < contest.Begin || DateTime.Now >= contest.End)
+                        return Content("Insufficient permissions");
+                    if (contest.ContestProblems.Where(x => x.ProblemID == problem_id).Count() == 0)
+                        return Content("Insufficient permissions");
+                    if (!Helpers.Contest.UserInContest(user.ID, contest_id.Value))
+                        return Content("Insufficient permissions");
+                    ContestFormat[] SubmitAnyTime = { ContestFormat.ACM, ContestFormat.OI };
+                    if (DateTime.Now < contest.Begin && user.Role < UserRole.Master && contest.UserID != user.ID)
+                    {
+                        return Content("Insufficient permissions");
+                    }
+                    if (contest.Format == ContestFormat.Codeforces && (from l in DbContext.Locks where l.ProblemID == problem_id && user.ID == l.UserID && l.ContestID == contest_id.Value select l).Count() > 0 && DateTime.Now < contest.End)
+                    {
+                        return Content("Locked");
+                    }
+                }
+
+                var status = new Status
+                {
+                    Code = code,
+                    LanguageAsInt = language_id,
+                    ProblemID = problem_id,
+                    UserID = user.ID,
+                    Time = DateTime.Now,
+                    Result = JudgeResult.Pending,
+                    ContestID = contest_id,
+                    Score = 0,
+                    TimeUsage = 0,
+                    MemoryUsage = 0
+                };
+                DbContext.Statuses.Add(status);
+                problem.SubmitCount++;
+                user.SubmitCount++;
+                var submitlist = Helpers.AcList.GetList(user.SubmitList);
+                submitlist.Add(problem.ID);
+                user.SubmitList = Helpers.AcList.ToString(submitlist);
+                var testcase_ids = (from tc in problem.TestCases
+                                    where tc.Type != TestCaseType.Sample
+                                    orderby tc.Type ascending
+                                    select tc.ID).ToList();
+                if (contest_id != null)
+                {
+                    if (DateTime.Now < contest.Begin || DateTime.Now >= contest.End || contest.Format == ContestFormat.ACM || contest.Format == ContestFormat.OI)
+                    {
+                        testcase_ids = (from tc in problem.TestCases
+                                        where tc.Type != TestCaseType.Sample
+                                        orderby tc.Type ascending
+                                        select tc.ID).ToList();
+                    }
+                    else if (contest.Format == ContestFormat.Codeforces)
+                    {
+                        testcase_ids = (from tc in problem.TestCases
+                                        where tc.Type == TestCaseType.Unilateralism
+                                        orderby tc.Type ascending
+                                        select tc.ID).ToList();
+                        var statuses = (from s in DbContext.Statuses
+                                        where s.ContestID == contest_id.Value
+                                        && s.UserID == user.ID
+                                        select s).ToList();
+                        foreach (var s in statuses)
+                        {
+                            if (s.JudgeTasks == null) continue;
+                            foreach (var jt in s.JudgeTasks)
+                            {
+                                testcase_ids.Add(jt.TestCaseID);
+                            }
+                        }
+                        testcase_ids = testcase_ids.Distinct().ToList();
+                    }
+                    foreach (var id in testcase_ids)
+                    {
+                        DbContext.JudgeTasks.Add(new JudgeTask
+                        {
+                            StatusID = status.ID,
+                            TestCaseID = id,
+                            Result = JudgeResult.Pending,
+                            MemoryUsage = 0,
+                            TimeUsage = 0,
+                            Hint = ""
+                        });
+                    }
+                    DbContext.SaveChanges();
+                    foreach (var jt in status.JudgeTasks)
+                    {
+                        try
+                        {
+                            var group = SignalR.JudgeHub.GetNode();
+                            if (group == null) return Content("No Online Judger");
+                            SignalR.JudgeHub.context.Clients.Group(group).Judge(new CodeComb.Judge.Models.JudgeTask(jt));
+                            SignalR.JudgeHub.ThreadBusy(group);
+                        }
+                        catch { }
+                    }
+                    SignalR.UserHub.context.Clients.Group("Status").onStatusCreated(new vStatus(status));//推送新状态
+                    if (contest.Format == ContestFormat.OI && DateTime.Now >= contest.Begin && DateTime.Now < contest.End)
+                        return Content("OI");
+                }
+                else // 不是比赛任务
+                {
+                    foreach (var id in testcase_ids)
+                    {
+                        DbContext.JudgeTasks.Add(new JudgeTask
+                        {
+                            StatusID = status.ID,
+                            TestCaseID = id,
+                            Result = JudgeResult.Pending,
+                            MemoryUsage = 0,
+                            TimeUsage = 0,
+                            Hint = ""
+                        });
+                    }
+                    DbContext.SaveChanges();
+                    if (string.IsNullOrWhiteSpace(problem.SpecialJudge) && (status.Language == Language.C || status.Language == Language.Cxx || status.Language == Language.Pascal)) // 如果是c,c++,pascal则由JoyOI接管评测
+                    {
+                        var sourceName = "Main."; // 拼接选手源代码文件名
+                        if (status.Language == Language.C)
+                            sourceName += "c";
+                        else if (status.Language == Language.Cxx)
+                            sourceName += "cpp";
+                        else
+                            sourceName += "pas";
+
+                        var client = new ManagementServiceClient("https://mgmtsvc.1234.sh", @"D:\Tyvj\webapi-client.pfx", "123456");
+                        var blobs = new List<BlobInfo>(20);
+                        blobs.Add(new BlobInfo { Id = Guid.Parse("0083c7bd-7c14-1035-82ec-54eca0c82300"), Name = "Validator.out" }); // 将标准比较器放入文件集合中
+                        var sourceBlobId = await client.PutBlobAsync(sourceName, System.Text.Encoding.UTF8.GetBytes(status.Code)); // 将选手文件上传至Management Service
+                        blobs.Add(new BlobInfo { Id = sourceBlobId, Name = sourceName }); // 将选手代码放入文件集合中
+
+                        // 准备测试用例
+                        var caseIndex = 0;
+                        foreach (var jt in status.JudgeTasks)
+                        {
+                            if (string.IsNullOrWhiteSpace(jt.TestCase.InputBlobId)) // 如果Management Service没有该测试用例则上传
+                            {
+                                var inputCaseBlobId = await client.PutBlobAsync("case_input_" + jt.TestCaseID + ".txt", System.Text.Encoding.UTF8.GetBytes(jt.TestCase.Input));
+                                jt.TestCase.InputBlobId = inputCaseBlobId.ToString();
+                            }
+                            if (string.IsNullOrWhiteSpace(jt.TestCase.OutputBlobId)) // 如果Management Service没有该测试用例则上传
+                            {
+                                var outputCaseBlobId = await client.PutBlobAsync("case_output_" + jt.TestCaseID + ".txt", System.Text.Encoding.UTF8.GetBytes(jt.TestCase.Output));
+                                jt.TestCase.OutputBlobId = outputCaseBlobId.ToString();
+                            }
+                            blobs.Add(new BlobInfo { Id = Guid.Parse(jt.TestCase.InputBlobId), Name = "input_" + caseIndex + ".txt" });
+                            blobs.Add(new BlobInfo { Id = Guid.Parse(jt.TestCase.OutputBlobId), Name = "output_" + caseIndex + ".txt" });
+                            ++caseIndex;
+                        }
+                        var stateMachineId = await client.PutStateMachineInstanceAsync("TyvjJudgeStateMachine", "http://tyvj.cn", blobs); // 创建StateMachine实例
+                        status.StateMachineId = stateMachineId.ToString();
+                        DbContext.SaveChanges();
+                    }
+                    else
+                    {
+                        foreach (var jt in status.JudgeTasks)
+                        {
+                            try
+                            {
+                                var group = SignalR.JudgeHub.GetNode();
+                                if (group == null) return Content("No Online Judger");
+                                SignalR.JudgeHub.context.Clients.Group(group).Judge(new CodeComb.Judge.Models.JudgeTask(jt));
+                                SignalR.JudgeHub.ThreadBusy(group);
+                            }
+                            catch { }
+                        }
+                    }
+                    SignalR.UserHub.context.Clients.Group("Status").onStatusCreated(new vStatus(status));//推送新状态
+                }
+
+                SignalR.UserHub.context.Clients.Group("Status").onStatusChanged(new vStatus(status));
+                GC.Collect();
+            }
+            return Content("Done");
         }
 
         public ActionResult Show(int id)
